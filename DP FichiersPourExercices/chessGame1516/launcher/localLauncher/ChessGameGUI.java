16ec645d0240ebfe9828ea81a5e0452e691e2456
Nicolas Lebrun <nicolas.lebrun@cpe.fr>
2016-05-09 15:19:40 +0200

    Update GUI

--- DP FichiersPourExercices/chessGame1516/launcher/localLauncher/ChessGameGUI.java
+++ DP FichiersPourExercices/chessGame1516/launcher/localLauncher/ChessGameGUI.java
@@ -9,6 +9,14 @@ import java.awt.*;
  * Created by MSIgamer on 29/04/2016.
  */
 public class ChessGameGUI extends JFrame {
-    public ChessGameGUI(String s, ChessGameControlers chessGameControler, Dimension dim) {
+
+    //new ChessGameGUI("Jeu d'échec", chessGameControler,  dim)
+    private String gameName;
+    private ChessGameControlers chessGameControlers;
+
+    public ChessGameGUI(String gameName, ChessGameControlers chessGameControlers, Dimension dim) {
+        this.gameName = gameName;
+        this.chessGameControlers = chessGameControlers;
+        this.setPreferredSize(dim);
     }
 }
